9b92e72039befc157d867e97ddd776e1bf2d5f0e
Bakhshali <tu78rfrfu@code.edu.az>
2022-04-24 06:46:18 +0400

    Task added

---
+++ WeekendExtention/Extention.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeekendExtention
+{
+    internal class Extention
+    {
+        private static int _id = 1000;
+
+        public int Id { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string Subject { get; set; }
+
+        public string GroupName { get; set; }
+
+        public Extention(string sub)
+        {
+            Subject = sub;
+            _id++;
+            Id = _id;
+        }
+
+        public string getSubject(string sub)
+        {
+            return char.ToUpper(sub[0]).ToString() + char.ToUpper(sub[1]).ToString();
+        }
+    }
+}
---
+++ WeekendExtention/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WeekendExtention
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sub = "Programming";
+            string sub1 = "Design";
+            string sub2 = "Enginer";
+
+            Extention extention = new Extention(sub);
+            Extention extention1 = new Extention(sub1);
+            Extention extention2 = new Extention(sub2);
+
+            Console.WriteLine(extention.getSubject(sub) + extention.Id);
+            Console.WriteLine(extention1.getSubject(sub1) + extention1.Id);
+            Console.WriteLine(extention2.getSubject(sub2) + extention2.Id);
+        }
+    }
+}
